99f6eac31694ef6eefea200133dd0fe2ef0d9176
Tristan Haroldsen <124015629+TristanRH-90@users.noreply.github.com>
2023-02-09 13:19:54 -0800

    Add Loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,12 @@ let pokemonList = [
     { name: "Charmeleon", height: 3.07, type: ["Fire"] },
     { name: "Charizard", height: 5.07, type: ["Fire","Flying"] },
     { name: "Charmander", height: 2.00, type: ["Fire"] }
-]
+];
 
+
+for (let i = 0; i < pokemonList[i].length; i++)
+    if (pokemonList[i].height <5) {
+        console.log(pokemonList[i].name + " WOW Thats huge");
+    } 
+
+document.write(pokemonList)
