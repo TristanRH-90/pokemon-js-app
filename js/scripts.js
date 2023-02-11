fab4f66b8aa82fabbdd3a525cae6b1f6555c9021
Tristan Haroldsen <124015629+TristanRH-90@users.noreply.github.com>
2023-02-10 17:30:46 -0800

    Updates to JS to display for loop on index.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 let pokemonList = [
-    { name: "Charmeleon", height: 3.07, type: ["Fire"] },
-    { name: "Charizard", height: 5.07, type: ["Fire","Flying"] },
-    { name: "Charmander", height: 2.00, type: ["Fire"] }
+    { 
+    name: "Charmeleon", 
+    height: 3.07,
+    types: ["Fire"] 
+    },
+    { 
+    name: "Charizard", 
+    height: 5.07,
+    types: ["Fire","Flying"] 
+    },
+    { 
+    name: "Charmander", 
+    height: 2.00, 
+    types: ["Fire"] }
 ];
 
-
-for (let i = 0; i < pokemonList[i].length; i++)
+for (let i = 0; i < pokemonList[i].length; i++) {
     if (pokemonList[i].height <5) {
         console.log(pokemonList[i].name + " WOW Thats huge");
     } 
-
-document.write(pokemonList)
+}
