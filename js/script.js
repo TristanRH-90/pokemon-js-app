18c3add62670672de631c88c009ff5f6415431a7
Tristan Haroldsen <124015629+TristanRH-90@users.noreply.github.com>
2023-02-18 15:31:47 -0800

    CSS updates, attempt to fix search issue, added comments to JS

--- js/script.js
+++ js/script.js
@@ -3,18 +3,40 @@ let pokemonRepository = (function() {
   let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
   let loadBar = document.querySelector(".lds-dual-ring");
 
- 
+        //This function gets all Pokemon from the API
   function getAll() {
       return pokemonList;
   }
 
-
+        // This function returns all Pokemon and pushes them to the Array..
   function add(pokemon) {
       if (typeof pokemon === "object" && "name" in pokemon && "detailsUrl" in pokemon) {
           pokemonList.push(pokemon);
       }
   }
 
+        // This function adds all pokemon details from the API.
+  function loadList() {
+    showLoadingMessage();
+    return fetch(apiUrl).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        json.results.forEach(function(item) {
+            let pokemon = {
+                name: item.name,
+                detailsUrl: item.url
+            };
+            add(pokemon);
+        });
+    }).catch(function(e) {
+        console.error(e);
+    }).finally(function() {
+        hideLoadingMessage();
+    });
+}
+
+
+        // This Function Creates a button by Pokemon name.
   function addListItem(pokemon) {
     let uPokemonList = $(".pokemon-list");
     let listItem = $('<li class="group-list-item"></li>');
@@ -29,32 +51,26 @@ let pokemonRepository = (function() {
     });
 }
 
-  
-  function getByName(search) {
-      return pokemonList.filter(function(pokemon) {
-          return pokemon.name.toLowerCase().includes(search.toLowerCase());
-      });
-  }
 
-  
-  function searchBar() {
-    let input= document.querySelector('.search-input');
-    let pokemonList2= document.querySelector('.pokemon-list')
-    let pokemonElements= pokemonList2.getElementsByTagName('li')
-    for (let i=0; i< pokemonElements.length; i++) {
-        pokemonElements[i].classList.remove('hide')
+    function getByName(search) {
+        return pokemonList.filter(function(pokemon) {
+            return pokemon.name.toLowerCase().includes(search.toLowerCase());
+        });
     }
 
-    for (let i=0; i< pokemonElements.length; i++) {
-        if(input.value=== ''){
-        } else if (pokemonElements[i].innerText.indexOf(input.value)) {
-            pokemonElements[i].classList.add('hide')
+  
+        // This function creates a search feature. 
+        function searchBar() {
+            let input= document.querySelector('.search-input');
+            let pokemonList2= document.querySelector('.pokemon-list')
+            let pokemonElements= pokemonList2.getElementsByTagName('li')
+            for (let i=0; i< pokemonElements.length; i++) {
+                pokemonElements[i].classList.remove('hide')
+            }
         }
-    }        
-}
+    
 
-
-
+        // This function loads pokemon details from the API.
   function loadDetails(item) {
     let url = item.detailsUrl;
     return fetch(url).then(function(response) {
@@ -67,38 +83,36 @@ let pokemonRepository = (function() {
     }).catch(function(e) {
         console.error(e);
     });
+    }
+
+ // This function shows the Pokemon Model on the Websitwe.
+ function showModal(pokemon) {
+    let types = "";
+    pokemon.types.forEach(function(type) {
+        types += type.type.name + " ";
+    });
+
+    let modalTitile = $(".modal-title");
+    let modalBody = $(".modal-body");
+
+    modalTitile.empty();
+    modalBody.empty();
+
+    let image = $('<img class="pokemon-img" src="' + pokemon.imageUrl + '" />');
+
+    modalTitile.append(pokemon.name);
+    modalBody.append(`<img class="modal-img" src="${pokemon.imageUrl}">`);
+    modalBody.append(`<p>Weight: ${pokemon.weight}</p>`);
+    modalBody.append(`<p>Height: ${pokemon.height}</p>`);
+    modalBody.append(`<p>Types: ${types}</p>`);
 }
 
-
-
-function showDetails(pokemon) {
-  loadDetails(pokemon).then(function() {
-      showModal(pokemon);
-  });
-}
-
-
-
-  function loadList() {
-      showLoadingMessage();
-      return fetch(apiUrl).then(function(response) {
-          return response.json();
-      }).then(function(json) {
-          json.results.forEach(function(item) {
-              let pokemon = {
-                  name: item.name,
-                  detailsUrl: item.url
-              };
-              add(pokemon);
-          });
-      }).catch(function(e) {
-          console.error(e);
-      }).finally(function() {
-          hideLoadingMessage();
-      });
-  }
-
-
+        // This function logs the pokemon details to the Console.
+    function showDetails(pokemon) {
+    loadDetails(pokemon).then(function() {
+        showModal(pokemon);
+    });
+    }
  
   function showLoadingMessage() {
       loadBar.classList.remove("lds-dual-ring-hidden");
@@ -111,29 +125,7 @@ function showDetails(pokemon) {
       loadBar.classList.add("lds-dual-ring-hidden");
   }
 
-
-  function showModal(pokemon) {
-      let types = "";
-      pokemon.types.forEach(function(type) {
-          types += type.type.name + " ";
-      });
-
-      let modalTitile = $(".modal-title");
-      let modalBody = $(".modal-body");
-
-      modalTitile.empty();
-      modalBody.empty();
-
-      let image = $('<img class="pokemon-img" src="' + pokemon.imageUrl + '" />');
-
-      modalTitile.append(pokemon.name);
-      modalBody.append(`<img class="modal-img" src="${pokemon.imageUrl}">`);
-      modalBody.append(`<p>Height: ${pokemon.height}</p>`);
-      modalBody.append(`<p>Weight: ${pokemon.weight}</p>`);
-      modalBody.append(`<p>Types: ${types}</p>`);
-  }
-
- 
+        // This funtion load the Pokemon from the API to the Website. 
   function loadAll() {
       loadList().then(function() {
           getAll().forEach(function(pokemon) {
@@ -143,17 +135,18 @@ function showDetails(pokemon) {
   }
 
   
+  
   return {
       getAll: getAll,
       add: add,
       getByName: getByName,
       addListItem: addListItem,
       loadList: loadList,
-      searchBar: searchBar,
       loadDetails: loadDetails,
       showDetails: showDetails,
       showModal: showModal,
-      loadAll: loadAll
+      loadAll: loadAll,
+      searchBar: searchBar,
       
   };
 })();
